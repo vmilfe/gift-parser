9c6e071a30f0beb3a71bb52c7f827412d676a8f7
vmilfe <ilyargat@gmail.com>
2025-01-16 22:22:19 +0300

    init all

---
+++ src/gifts/__init__.py
@@ -0,0 +1,7 @@
+from .parser import get_client, get_user_gifts, parse_members
+
+__all__ = [
+    "get_client",
+    "get_user_gifts",
+    "parse_members"
+]
---
+++ src/handlers/__init__.py
@@ -0,0 +1,5 @@
+from .default import router
+
+__all__ = [
+    "router"
+]
